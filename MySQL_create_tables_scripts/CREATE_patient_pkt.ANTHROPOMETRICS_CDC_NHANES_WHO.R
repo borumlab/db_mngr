fea7332ff17d8b7e6b05396f27c2b74dcd206290
jurate162001 <jurate162001@ufl.edu>
2017-03-14 16:34:49 -0400

    update

--- MySQL_create_tables_scripts/CREATE_patient_pkt.ANTHROPOMETRICS_CDC_NHANES_WHO.R
+++ MySQL_create_tables_scripts/CREATE_patient_pkt.ANTHROPOMETRICS_CDC_NHANES_WHO.R
@@ -5,7 +5,7 @@ setwd('G:/Grad Notebooks_E/e6/growth')
 
 #Step 2: upload CDC reference file
 CDC <- read.csv('ANTHROPOMETRICS_CDC_REFERENCES_SOURCE_08012016.txt', header=TRUE, sep="\t", na.strings=c("","NA"))
-NHANES <- read.csv('ANTHROPOMETRICS_NHANES_REFERENCES_SOURCE_08012016.txt', header=TRUE, sep="\t", na.strings=c("","NA"))
+NHANES <- read.csv('ANTHROPOMETRICS_NHANES_REFERENCES_SOURCE_11182016.txt', header=TRUE, sep="\t", na.strings=c("","NA"))
 WHO <- read.csv('ANTHROPOMETRICS_WHO_REFERENCES_SOURCE_08012016.txt', header=TRUE, sep="\t", na.strings=c("","NA"))
 
 #Step 3: upload references into MySQL
@@ -15,7 +15,7 @@ library(RMySQL)
 connect <- dbConnect(MySQL(),user='jurate162001',password='Taurage54',dbname='patient_pkt',host='if-srvv-borum')
 
 dbWriteTable(connect,value=CDC,name="ANTHROPOMETRICS_CDC_REFERENCES_SOURCE_08012016",append=TRUE)
-dbWriteTable(connect,value=NHANES,name="ANTHROPOMETRICS_NHANES_REFERENCES_SOURCE_08012016",append=TRUE)
+dbWriteTable(connect,value=NHANES,name="ANTHROPOMETRICS_NHANES_REFERENCES_SOURCE_11182016",append=TRUE)
 dbWriteTable(connect,value=WHO,name="ANTHROPOMETRICS_WHO_REFERENCES_SOURCE_08012016",append=TRUE)
 
 all_cons <- dbListConnections(MySQL())
